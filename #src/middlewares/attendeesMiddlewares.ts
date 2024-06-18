69c07905941f3b7e7620c7c823eea94cd082d965
Daniel <daniel.vn.rus@gmail.com>
2024-06-18 18:04:45 +0300

    Upd минимум агресии

--- #src/middlewares/attendeesMiddlewares.ts
+++ #src/middlewares/attendeesMiddlewares.ts
@@ -3,52 +3,52 @@ import { inputValidationMiddleware } from "./inputValidationMiddleware";
 import { companyExistsMiddlewares } from "./companyExistsMiddlewares";
 
 export const nameMiddleware = body("name")
-    .isLength({ min: 3, max: 30 })
+    .isLength({ min: 2, max: 30 })
     .withMessage({
-        ru: "Длина Имени от 3-х до 30-ти символов!",
-        en: "The length of the Name is from 3 to 30 characters!",
+        ru: "Длина Имени от 2-х до 30-ти символов",
+        en: "The length of the Name is from 2 to 30 characters",
     });
 
 export const surnameMiddleware = body("surname")
-    .isLength({ min: 3, max: 30 })
+    .isLength({ min: 2, max: 30 })
     .withMessage({
-        ru: "Длина Фамилии от 3-х до 30-ти символов!",
-        en: "The length of the Lastname is from 3 to 30 characters!",
+        ru: "Длина Фамилии от 2-х до 30-ти символов",
+        en: "The length of the Lastname is from 2 to 30 characters",
     });
 
 export const lastnameMiddleware = body("lastname")
     .optional()
-    .isLength({ min: 3, max: 30 })
+    .isLength({ min: 2, max: 30 })
     .withMessage({
-        ru: "Длина Отчества от 3-х до 30-ти символов!",
-        en: "The length of the Patronymic is from 3 to 30 characters!",
+        ru: "Длина Отчества от 2-х до 30-ти символов",
+        en: "The length of the Patronymic is from 2 to 30 characters",
     });
 
 export const organizationMiddleware = body("organization")
     .isLength({ min: 3, max: 20 })
     .withMessage({
-        ru: "Некорректная организация!",
-        en: "Incorrect organization!",
+        ru: "Некорректная организация",
+        en: "Incorrect organization",
     });
 
 export const gradeMiddleware = body("grade")
     .isLength({ min: 3, max: 60 })
     .withMessage({
-        ru: "Длина Должности от 3-х до 60-ти символов!",
-        en: "The length of the Post is from 3 to 60 characters!",
+        ru: "Длина Должности от 3-х до 60-ти символов",
+        en: "The length of the Post is from 3 to 60 characters",
     });
 
 const mailMiddleware = body("mail").isEmail().withMessage({
-    ru: "Некорректный Email!",
-    en: "Incorrect Email!",
+    ru: "Некорректный Email",
+    en: "Incorrect Email",
 });
 
 const phoneMiddleware = body("phone")
     .isLength({ min: 3, max: 30 })
     .custom(notHasLetters)
     .withMessage({
-        ru: "Некорректный номер телефона!",
-        en: "Incorrect phone number!",
+        ru: "Некорректный номер телефона",
+        en: "Incorrect phone number",
     });
 
 const countryMiddleware = body("country")
