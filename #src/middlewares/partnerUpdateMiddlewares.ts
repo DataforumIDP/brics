c4085775dd182cb67fd6df8cf37199e911d78cdc
Daniel <daniel.vn.rus@gmail.com>
2024-06-25 18:55:57 +0300

    Update

--- #src/middlewares/partnerUpdateMiddlewares.ts
+++ #src/middlewares/partnerUpdateMiddlewares.ts
@@ -7,26 +7,22 @@ import { companyExistsMiddlewares } from "./companyExistsMiddlewares";
 const reminderMiddleware = body("reminder")
     .optional()
     .isLength({ min: 3, max: 200 })
-    .withMessage("Длинна 3-200 символов");
+    .withMessage("Длинна до 200 символов");
 
 const descriptionMiddleware = body("description")
     .optional()
-    .isLength({ min: 3, max: 200 })
-    .withMessage("Длинна 3-200 символов");
+    .isLength({ max: 200 })
+    .withMessage("Длинна до 200 символов");
 
 const contactsMiddleware = body("contacts")
     .optional()
-    .custom(items => {        
-        if (items === null) return true
-        if ((items as string[]).every(item=>item.length<200)) return true
-        throw new Error()
-    })
-    .withMessage("Максимальная длинна одного контакта 200 символов");
+    .isLength({ max: 200 })
+    .withMessage("Длинна до 200 символов");
 
 const siteMiddleware = body("site")
     .optional()
-    .isLength({ min: 3, max: 200 })
-    .withMessage("Длинна 3-200 символов");
+    .isLength({ max: 200 })
+    .withMessage("Длинна до 200 символов");
 
 export const partnerUpdateMiddlewares = [
     tokenAuthorizeCheck(),
@@ -36,5 +32,5 @@ export const partnerUpdateMiddlewares = [
     contactsMiddleware,
     siteMiddleware,
     inputValidationMiddleware,
-    companyExistsMiddlewares({optional: true}),
+    companyExistsMiddlewares({ optional: true }),
 ];
