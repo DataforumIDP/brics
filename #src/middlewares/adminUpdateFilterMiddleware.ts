0742463dcba68418957363dda890759d6a296230
John Doe <daniel.vn.rus@gmail.com>
2024-07-25 21:17:52 +0300

    Upd

--- #src/middlewares/adminUpdateFilterMiddleware.ts
+++ #src/middlewares/adminUpdateFilterMiddleware.ts
@@ -7,12 +7,19 @@ export function adminUpdateFilterMiddleware(
     next: NextFunction
 ) {
     const data = req.body;
+
+    console.log('Body: ', data);
+    
+
     const updateKeys = Object.keys(data).filter(
         (key) => allowedUpdates.includes(key) && (data[key] != "" || data[key] === null)
     );
     
     let result = {}
     updateKeys.map(item=> (result[item] = data[item]))
+
+    console.log('Filtred: ', result);
+
     req.body = result    
     next()
 }
